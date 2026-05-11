c57fdafde97d98b08c344fd0adabe6da6ddafb34
Jorge <jorgealvarezfrancino@gmail.com>
2026-05-11 17:07:34 +0200

    git commit -m "remove legacy locale redirects"

--- next.config.ts
+++ next.config.ts
@@ -15,30 +15,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/es',
-        permanent: true,
-      },
-      {
-        source: '/servicios',
-        destination: '/es/servicios',
-        permanent: true,
-      },
-      {
-        source: '/contacto',
-        destination: '/es/contacto',
-        permanent: true,
-      },
-      {
-        source: '/equipo',
-        destination: '/es/equipo',
-        permanent: true,
-      },
-    ];
-  },
 };
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
